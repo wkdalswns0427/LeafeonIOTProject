c84178dd57d861e9718db68d3ba6f28288a2455d
MinJun <pawpaw0427@gmail.com>
2023-01-14 16:16:31 +0900

    rtos

--- Arduino-IDE/DEMO/IND_TEST/IND_TEST.ino
+++ Arduino-IDE/DEMO/IND_TEST/IND_TEST.ino
@@ -1,48 +1,40 @@
-// This is prototype ESP32-LEAFEON project Arduino version 
-// refer to this link
-//https://github.com/olikraus/u8glib/wiki/userreference#setcolorindex
-// in case of ssh1106 graphic driver
-
+// #include <Arduino.h>
+#include "WiFi.h"
+#include "time.h"
 #include "DFRobot_BME280.h"
 #include "DFRobot_CCS811.h"
 #include "PMS.h"
-#include <SPI.h>
-#include <Wire.h>
-#include <WiFi.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <U8glib.h>
 
-// for wifi connection
-const char* ssid = "AP Address";        //U+Net850C
-const char* password = "**********";    //C87568BJ$F//12345678///csdowu38
-
-// SSD1306 driver OLED elements
-// but might need to change to SSH1106 in that case... either
-U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_NONE) ; // or U8G_I2C_OPT_NO_ACK
-
-#define SleftEEN_WIDTH 128 // OLED display width, in pixels
-#define SleftEEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     -1 
-#define SleftEEN_ADDRESS 0x3C // address might be 0x3D, 0x7A or 0x3C
-Adafruit_SSD1306 display(SleftEEN_WIDTH, SleftEEN_HEIGHT, &Wire, OLED_RESET);
-#define OLED_DATA "LeafeonPJ 0.0"
-
-#define I2C_SDA 21
-#define I2C_SCL 22
+#define RUNNING_CORE 1
+#define SDA_PIN 21
+#define SCL_PIN 22
 #define TX_PIN 1
 #define RX_PIN 3
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET     -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-// elements of SEN0335 & PMS7003
+const char* ssid = "Roboin";        //U+Net850C
+const char* password = "roboin1234";    //C87568BJ$F//12345678///csdowu38
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 32400;
+const int   daylightOffset_sec = 0;
+
+// elements of SEN0335 : I2C
 typedef DFRobot_BME280_IIC    BME;   
 typedef DFRobot_CCS811        CCS;
 BME    bme(&Wire, 0x76);   
 CCS CCS811(&Wire, 0x5A);
+#define SEA_LEVEL_PRESSURE    1015.0f
+
+// PMS7003 sensor : UART - using PMSSerial
 PMS pms(Serial1);
 PMS::DATA pmsdata;
 
-#define SEA_LEVEL_PRESSURE    1015.0f
-
+// data cluster
 typedef struct SENDATA{
     float temperature;
     float pressure;
@@ -50,12 +42,38 @@ typedef struct SENDATA{
     float humidity;
     uint16_t eCO2;
     uint16_t eTVOC;
-    float pm1_0;
-    float pm2_5;
-    float pm10_0;
+    uint16_t PM_AE_UG_1_0;
+    uint16_t PM_AE_UG_2_5;
+    uint16_t PM_AE_UG_10_0;
 }SENDATA;
 
 uint baseline = 0;
+
+void printLocalTime() {
+    struct tm timeinfo;
+    if(!getLocalTime(&timeinfo)) {
+        Serial.println("Failed to obtain time");
+        return;
+    }
+    // Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+    display.println(&timeinfo, "%H:%M:%S");
+}
+
+void displayInitialTime(){
+    struct tm timeinfo;
+    if(!getLocalTime(&timeinfo)) {
+        Serial.println("Failed to obtain time");
+        return;
+    }
+
+    display.clearDisplay();
+    display.setCursor(0, 24);
+    display.setTextSize(1);
+    display.println(&timeinfo, "%A, %B %d");
+    display.println(&timeinfo, "%H:%M:%S");
+    display.display();
+    delay(3000);
+}
 
 // show last sensor operate status
 void printLastOperateStatus(BME::eStatus_t eStatus)
@@ -77,9 +95,6 @@ void setupBME(){
         delay(2000);
     }
     Serial.println("bme begin success");
-    display.clearDisplay();
-    display.print("bme begin");
-    display.display();
 }
 
 void setupCCS(){
@@ -91,10 +106,6 @@ void setupCCS(){
     delay(1000);
     Serial.println("ccs set baseline");
     if(CCS811.checkDataReady() == true){
-        /*!
-         * @brief Set baseline
-         * @return baseline in clear air
-         */
         baseline = CCS811.readBaseLine();
         Serial.println(baseline, HEX);
         
@@ -103,82 +114,41 @@ void setupCCS(){
     }
 }
 
-void setupPMS(){
-  Serial1.begin(PMS::BAUD_RATE, SERIAL_8N1, 3, 1);
-}
-
-void setupALL(){
-  setupBME();
-  setupCCS();
-  setupPMS();
-}
-
 void setupOLED(){
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SleftEEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); 
-  }
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;); // Don't proceed, loop forever
+    }
+    display.clearDisplay();
 
-  delay(100); 
-  display.clearDisplay();
-
-  //display.setRotation(2); // Uncomment to rotate display 180 degrees
-  display.setTextSize(1);  
-  display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.cp437(true); 
-
-  display.setCursor(0, 0);  // 0,0 / 8 / 16 / 24
-  display.print(OLED_DATA);
-  display.setCursor(0, 8); 
-  display.print("OLED INIT");
-  display.display();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(4, 28);
+    display.println("** Begin Leafeon **");
+    display.display();
+    delay(2000);
 }
 
-void setupOLED_ssh1106(){
-  //u8g.setRot180();
-  u8g.setFont(u8g_font_6x12);
-  u8g.setFontRefHeightText();
-  u8g.setFontPosTop();
-
-  if ( u8g.getMode() == U8G_MODE_R3G3B2 ) {
-    u8g.setColorIndex(255); // white
-  }
-
-  // test display
-  do{
-    // drawStr(uint8_t x, uint8_t y, string msg)
-    u8g.drawStr(0, 22, "OLED INIT")  ;
-  }while(u8g.nextPage());
+void setupWiFi(){
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+    }
+    resetDisplay();
+    display.println("WIFI CONNECTED");
+    display.print("IP :"); display.println(WiFi.localIP());
+    delay(2000);
 }
 
-void setupWIFI(){
-  WiFi.begin(ssid, password);
-  Serial.println("");
-
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-
-  display.clearDisplay();
-  display.setCursor( 0,  0);
-  display.print("Connected: ");
-  display.println(ssid);
-  display.setCursor( 0,  8);
-  display.print("IP: ");
-  display.print(WiFi.localIP());
-  display.display();
+void resetDisplay(){
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.setTextSize(1);
 }
 
-// int *a = (int *)malloc(sizeof(int) * 갯수);
-// int *a = new int[갯수];
-float* readBME(){
-    // float data[4];
+float *readBME(){
     float *data = (float*)malloc(sizeof(float)*4);
+    
     data[0] = bme.getTemperature();
     data[1] = bme.getPressure();
     data[2] = bme.calAltitude(SEA_LEVEL_PRESSURE, data[1]);
@@ -187,209 +157,88 @@ float* readBME(){
     return data;
 }
 
-uint16_t* readCCS(){
+uint16_t *readCCS(){
     uint16_t *data = (uint16_t*)malloc(sizeof(uint16_t)*2);
+    
     if(CCS811.checkDataReady() == true){
         data[0] = CCS811.getCO2PPM();
         data[1] = CCS811.getTVOCPPB();
     }
-
     return data;
 }
 
-float* readPMS(){
-  float *data= (float*)malloc(sizeof(float)*3);
+uint16_t *readPMS(){
+  uint16_t *data = (uint16_t*)malloc(sizeof(uint16_t)*3);
+    
   while (Serial1.available()) { Serial1.read(); }
   pms.requestRead();
-  if (pms.readUntil(pmsdata))
-  {
-    data[0] = pmsdata.PM_AE_UG_1_0;
-    data[1] = pmsdata.PM_AE_UG_2_5;
-    data[2] = pmsdata.PM_AE_UG_10_0;
+  if (pms.readUntil(pmsdata)){
+      data[0] = pmsdata.PM_AE_UG_1_0;
+      data[1] = pmsdata.PM_AE_UG_2_5;
+      data[2] = pmsdata.PM_AE_UG_1_0;
   }
   return data;
 }
 
-int main(){
+// main function
+void sensortask( void *pvParameters){
+
+  (void) pvParameters;
+
   float* BMEdata = readBME();
   uint16_t* CCSdata = readCCS();
-  float* PMSdata = readPMS();
+  uint16_t* PMSdata = readPMS();
   SENDATA SENRESULT = 
-  {
-    BMEdata[0],BMEdata[1],BMEdata[2],BMEdata[3],
-    CCSdata[0],CCSdata[1],
-    PMSdata[0], PMSdata[1], PMSdata[2]
-  };
+          {BMEdata[0], BMEdata[1], BMEdata[2], BMEdata[3]
+          ,CCSdata[0], CCSdata[1]
+          ,PMSdata[0], PMSdata[1], PMSdata[2]};
 
-  Serial.println();
-  Serial.println("======== start print ========");
-  Serial.print("temperature (unit Celsius): "); Serial.println(SENRESULT.temperature);
-  Serial.print("pressure (unit pa):         "); Serial.println(SENRESULT.pressure);
-  Serial.print("altitude (unit meter):      "); Serial.println(SENRESULT.altitude);
-  Serial.print("humidity (unit percent):    "); Serial.println(SENRESULT.humidity);
-  Serial.print("CO2(ppm): "); Serial.println(SENRESULT.eCO2);
-  Serial.print("TVOC(ppb): "); Serial.println(SENRESULT.eTVOC);
-  Serial.print("PM 1.0 (ug/m3): "); Serial.println(SENRESULT.pm1_0);
-  Serial.print("PM 2.5 (ug/m3): "); Serial.println(SENRESULT.pm2_5);
-  Serial.print("PM 10.0 (ug/m3): "); Serial.println(SENRESULT.pm10_0);
-  Serial.println("========  end print  ========");
+  //================= this part shall be either posting or OLED =================
+  resetDisplay();
+  printLocalTime();
+  display.print("temp(C): "); display.println(SENRESULT.temperature);
+  display.print("P(Pa): "); display.println(SENRESULT.pressure);
+//   display.print("alt(m):  "); display.println(SENRESULT.altitude);
+  display.print("hum(%): "); display.println(SENRESULT.humidity);
+  display.print("CO2(ppm): "); display.println(SENRESULT.eCO2);
+  display.print("TVOC(ppb): "); display.println(SENRESULT.eTVOC);
+  display.display();
+  delay(5000);
+  
+  resetDisplay();
+  display.println("micro particle(ug/m3)");
+  display.print("PM 1.0: "); display.println(SENRESULT.PM_AE_UG_1_0);
+  display.print("PM 2.5: "); display.println(SENRESULT.PM_AE_UG_2_5);
+  display.print("PM 10.0: "); display.println(SENRESULT.PM_AE_UG_10_0);
+  display.display();
+  delay(5000);
+  //==============================================================================
 
   CCS811.writeBaseLine(baseline);
-  delay(900);
-  return 0;
 }
 
 void setup()
 {
-  Serial.begin(115200);
-  Wire.begin(I2C_SDA, I2C_SCL);
-  Serial.println("======== start serial ========");
-  // setupOLED();
-  setupWIFI();
-  Serial.println("======== start IIC ========");
-  setupALL();
-  Serial.println("======== Setup Done ========");
+  Serial1.begin(PMS::BAUD_RATE, SERIAL_8N1, RX_PIN, TX_PIN);
+  Serial.begin(9600);
+  setupBME();
+  setupCCS();
+  setupOLED();
+  setupWiFi();
+  
+  // init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  displayInitialTime();
 
-  // // task attribute --> main : sensors
-  // xTaskCreate(
-  //   main,
-  //   "main",
-  //   1024,
-  //   NULL,
-  //   1,
-  //   NULL
-  // );
-  // need task for OTA
+  xTaskCreatePinnedToCore(
+        sensortask,
+        "sensortask",
+        4096,
+        NULL, // task function input
+        1,
+        NULL,
+        RUNNING_CORE
+    );
 }
 
-void loop()
-{
-    main();
-    delay(10000);
-}
-
-
-//--------------------- bewlow 22.12.21 legacy -------------------------------
-// #include "DFRobot_BME280.h"
-// #include "DFRobot_CCS811.h"
-// #define SEA_LEVEL_PRESSURE    1015.0f
-
-// // elements of SEN0335
-// typedef DFRobot_BME280_IIC    BME;   
-// typedef DFRobot_CCS811        CCS;
-// BME    bme(&Wire, 0x76);   
-// CCS CCS811(&Wire, 0x5A);
-
-// typedef struct{
-//     float temperature;
-//     float pressure;
-//     float altitude;
-//     float humidity;
-//     uint16_t eCO2;
-//     uint16_t eTVOC;
-// }SENDATA;
-
-// uint baseline = 0;
-
-// // show last sensor operate status
-// void printLastOperateStatus(BME::eStatus_t eStatus)
-// {
-//   switch(eStatus) {
-//   case BME::eStatusOK:    Serial.println("everything ok"); break;
-//   case BME::eStatusErr:   Serial.println("unknow error"); break;
-//   case BME::eStatusErrDeviceNotDetected:    Serial.println("device not detected"); break;
-//   case BME::eStatusErrParameter:    Serial.println("parameter error"); break;
-//   default: Serial.println("unknow status"); break;
-//   }
-// }
-
-// void setupBME(){
-//     Serial.println("bme read data test");
-//     while(bme.begin() != BME::eStatusOK) {
-//         Serial.println("bme begin faild");
-//         printLastOperateStatus(bme.lastOperateStatus);
-//         delay(2000);
-//     }
-//     Serial.println("bme begin success");
-// }
-
-// void setupCCS(){
-//     while(CCS811.begin() != 0){
-//             Serial.println("failed to init chip, please check if the chip connection is fine");
-//             delay(1000);
-//         }
-//     Serial.println("ccs begin success");
-//     delay(1000);
-//     Serial.println("ccs set baseline");
-//     if(CCS811.checkDataReady() == true){
-//         /*!
-//          * @brief Set baseline
-//          * @return baseline in clear air
-//          */
-//         baseline = CCS811.readBaseLine();
-//         Serial.println(baseline, HEX);
-        
-//     } else {
-//         Serial.println("Data is not ready!");
-//     }
-// }
-
-// // int *a = (int *)malloc(sizeof(int) * 갯수);
-// // int *a = new int[갯수];
-
-// float* readBME(){
-//     // float data[4];
-//     float *data = (float*)malloc(sizeof(float)*4);
-//     data[0] = bme.getTemperature();
-//     data[1] = bme.getPressure();
-//     data[2] = bme.calAltitude(SEA_LEVEL_PRESSURE, data[1]);
-//     data[3] = bme.getHumidity();
-
-//     return data;
-// }
-
-// uint16_t* readCCS(){
-//     uint16_t *data = (uint16_t*)malloc(sizeof(uint16_t)*2);
-//     if(CCS811.checkDataReady() == true){
-//         data[0] = CCS811.getCO2PPM();
-//         data[1] = CCS811.getTVOCPPB();
-//     }
-
-//     return data;
-// }
-
-// int main(){
-//   float* BMEdata = readBME();
-//   delay(100);
-//   uint16_t* CCSdata = readCCS();
-//   SENDATA SENRESULT = {BMEdata[0],BMEdata[1],BMEdata[2],BMEdata[3],CCSdata[0],CCSdata[1]};
-
-//   Serial.println();
-//   Serial.println("======== start print ========");
-//   Serial.print("temperature (unit Celsius): "); Serial.println(SENRESULT.temperature);
-//   Serial.print("pressure (unit pa):         "); Serial.println(SENRESULT.pressure);
-//   Serial.print("altitude (unit meter):      "); Serial.println(SENRESULT.altitude);
-//   Serial.print("humidity (unit percent):    "); Serial.println(SENRESULT.humidity);
-//   Serial.print("CO2(ppm): "); Serial.println(SENRESULT.eCO2);
-//   Serial.print("TVOC(ppb): "); Serial.println(SENRESULT.eTVOC);
-//   Serial.println("========  end print  ========");
-
-//   CCS811.writeBaseLine(baseline);
-//   delay(900);
-//   return 0;
-// }
-
-// void setup()
-// {
-//   Serial.begin(115200);
-//   setupBME();
-//   delay(100);
-//   setupCCS();
-//   Serial.println("======== Setup Done ========");
-// }
-
-// void loop()
-// {
-//     main();
-//     delay(10000);
-// }
+void loop(){}
