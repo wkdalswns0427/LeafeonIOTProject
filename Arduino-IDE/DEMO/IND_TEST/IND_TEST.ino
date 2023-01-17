4fd53666a36641377120ef981ba49fb39d7c7ea6
charmander1024 <68832065+wkdalswns0427@users.noreply.github.com>
2023-01-17 14:15:26 +0900

    include ota

--- Arduino-IDE/DEMO/IND_TEST/IND_TEST.ino
+++ Arduino-IDE/DEMO/IND_TEST/IND_TEST.ino
@@ -7,6 +7,11 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <Update.h>
+
 #define RUNNING_CORE 1
 #define SDA_PIN 21
 #define SCL_PIN 22
@@ -17,6 +22,7 @@
 #define OLED_RESET     -1
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+const char* host = "esp32";
 const char* ssid = "Roboin";        //U+Net850C
 const char* password = "roboin1234";    //C87568BJ$F//12345678///csdowu38
 const char* ntpServer = "pool.ntp.org";
@@ -34,6 +40,9 @@ CCS CCS811(&Wire, 0x5A);
 PMS pms(Serial1);
 PMS::DATA pmsdata;
 
+// web update server
+WebServer server(80);
+
 // data cluster
 typedef struct SENDATA{
     float temperature;
@@ -48,6 +57,76 @@ typedef struct SENDATA{
 }SENDATA;
 
 uint baseline = 0;
+
+/* Style */
+String style =
+"<style>#file-input,input{width:100%;height:44px;border-radius:4px;margin:10px auto;font-size:15px}"
+"input{background:#f1f1f1;border:0;padding:0 15px}body{background:#3498db;font-family:sans-serif;font-size:14px;color:#777}"
+"#file-input{padding:0;border:1px solid #ddd;line-height:44px;text-align:left;display:block;cursor:pointer}"
+"#bar,#prgbar{background-color:#f1f1f1;border-radius:10px}#bar{background-color:#3498db;width:0%;height:10px}"
+"form{background:#fff;max-width:258px;margin:75px auto;padding:30px;border-radius:5px;text-align:center}"
+".btn{background:#3498db;color:#fff;cursor:pointer}</style>";
+
+/* Login page */
+String loginIndex = 
+"<form name=loginForm>"
+"<h1>ESP32 Login</h1>"
+"<input name=userid placeholder='User ID'> "
+"<input name=pwd placeholder=Password type=Password> "
+"<input type=submit onclick=check(this.form) class=btn value=Login></form>"
+"<script>"
+"function check(form) {"
+"if(form.userid.value=='admin' && form.pwd.value=='admin')"
+"{window.open('/serverIndex')}"
+"else"
+"{alert('Error Password or Username')}"
+"}"
+"</script>" + style;
+ 
+/* Server Index Page */
+String serverIndex = 
+"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+"<input type='file' name='update' id='file' onchange='sub(this)' style=display:none>"
+"<label id='file-input' for='file'>   Choose file...</label>"
+"<input type='submit' class=btn value='Update'>"
+"<br><br>"
+"<div id='prg'></div>"
+"<br><div id='prgbar'><div id='bar'></div></div><br></form>"
+"<script>"
+"function sub(obj){"
+"var fileName = obj.value.split('\\\\');"
+"document.getElementById('file-input').innerHTML = '   '+ fileName[fileName.length-1];"
+"};"
+"$('form').submit(function(e){"
+"e.preventDefault();"
+"var form = $('#upload_form')[0];"
+"var data = new FormData(form);"
+"$.ajax({"
+"url: '/update',"
+"type: 'POST',"
+"data: data,"
+"contentType: false,"
+"processData:false,"
+"xhr: function() {"
+"var xhr = new window.XMLHttpRequest();"
+"xhr.upload.addEventListener('progress', function(evt) {"
+"if (evt.lengthComputable) {"
+"var per = evt.loaded / evt.total;"
+"$('#prg').html('progress: ' + Math.round(per*100) + '%');"
+"$('#bar').css('width',Math.round(per*100) + '%');"
+"}"
+"}, false);"
+"return xhr;"
+"},"
+"success:function(d, s) {"
+"console.log('success!') "
+"},"
+"error: function (a, b, c) {"
+"}"
+"});"
+"});"
+"</script>" + style;
 
 void printLocalTime() {
     struct tm timeinfo;
@@ -140,6 +219,52 @@ void setupWiFi(){
     delay(2000);
 }
 
+void setupWebServer(){
+      /*use mdns for host name resolution*/
+    if (!MDNS.begin(host)) { //http://esp32.local
+        Serial.println("Error setting up MDNS responder!");
+        while (1) {
+        delay(1000);
+        }
+    }
+    Serial.println("mDNS responder started");
+    /*return index page which is stored in serverIndex */
+    server.on("/", HTTP_GET, []() {
+        server.sendHeader("Connection", "close");
+        server.send(200, "text/html", loginIndex);
+    });
+    server.on("/serverIndex", HTTP_GET, []() {
+        server.sendHeader("Connection", "close");
+        server.send(200, "text/html", serverIndex);
+    });
+    /*handling uploading firmware file */
+    server.on("/update", HTTP_POST, []() {
+        server.sendHeader("Connection", "close");
+        server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+        ESP.restart();
+    }, []() {
+        HTTPUpload& upload = server.upload();
+        if (upload.status == UPLOAD_FILE_START) {
+        Serial.printf("Update: %s\n", upload.filename.c_str());
+        if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
+            Update.printError(Serial);
+        }
+        } else if (upload.status == UPLOAD_FILE_WRITE) {
+        /* flashing firmware to ESP*/
+        if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+            Update.printError(Serial);
+        }
+        } else if (upload.status == UPLOAD_FILE_END) {
+        if (Update.end(true)) { //true to set the size to the current progress
+            Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+        } else {
+            Update.printError(Serial);
+        }
+        }
+    });
+    server.begin();
+}
+
 void resetDisplay(){
     display.clearDisplay();
     display.setCursor(0, 0);
@@ -184,37 +309,38 @@ uint16_t *readPMS(){
 void sensortask( void *pvParameters){
 
   (void) pvParameters;
+    while(true){
+    float* BMEdata = readBME();
+    uint16_t* CCSdata = readCCS();
+    uint16_t* PMSdata = readPMS();
+    SENDATA SENRESULT = 
+            {BMEdata[0], BMEdata[1], BMEdata[2], BMEdata[3]
+            ,CCSdata[0], CCSdata[1]
+            ,PMSdata[0], PMSdata[1], PMSdata[2]};
 
-  float* BMEdata = readBME();
-  uint16_t* CCSdata = readCCS();
-  uint16_t* PMSdata = readPMS();
-  SENDATA SENRESULT = 
-          {BMEdata[0], BMEdata[1], BMEdata[2], BMEdata[3]
-          ,CCSdata[0], CCSdata[1]
-          ,PMSdata[0], PMSdata[1], PMSdata[2]};
+    //================= this part shall be either posting or OLED =================
+    resetDisplay();
+    printLocalTime();
+    display.print("temp(C): "); display.println(SENRESULT.temperature);
+    display.print("P(Pa): "); display.println(SENRESULT.pressure);
+    //   display.print("alt(m):  "); display.println(SENRESULT.altitude);
+    display.print("hum(%): "); display.println(SENRESULT.humidity);
+    display.print("CO2(ppm): "); display.println(SENRESULT.eCO2);
+    display.print("TVOC(ppb): "); display.println(SENRESULT.eTVOC);
+    display.display();
+    delay(5000);
+    
+    resetDisplay();
+    display.println("micro particle(ug/m3)");
+    display.print("PM 1.0: "); display.println(SENRESULT.PM_AE_UG_1_0);
+    display.print("PM 2.5: "); display.println(SENRESULT.PM_AE_UG_2_5);
+    display.print("PM 10.0: "); display.println(SENRESULT.PM_AE_UG_10_0);
+    display.display();
+    delay(5000);
+    //==============================================================================
 
-  //================= this part shall be either posting or OLED =================
-  resetDisplay();
-  printLocalTime();
-  display.print("temp(C): "); display.println(SENRESULT.temperature);
-  display.print("P(Pa): "); display.println(SENRESULT.pressure);
-//   display.print("alt(m):  "); display.println(SENRESULT.altitude);
-  display.print("hum(%): "); display.println(SENRESULT.humidity);
-  display.print("CO2(ppm): "); display.println(SENRESULT.eCO2);
-  display.print("TVOC(ppb): "); display.println(SENRESULT.eTVOC);
-  display.display();
-  delay(5000);
-  
-  resetDisplay();
-  display.println("micro particle(ug/m3)");
-  display.print("PM 1.0: "); display.println(SENRESULT.PM_AE_UG_1_0);
-  display.print("PM 2.5: "); display.println(SENRESULT.PM_AE_UG_2_5);
-  display.print("PM 10.0: "); display.println(SENRESULT.PM_AE_UG_10_0);
-  display.display();
-  delay(5000);
-  //==============================================================================
-
-  CCS811.writeBaseLine(baseline);
+    CCS811.writeBaseLine(baseline);
+    }
 }
 
 void setup()
@@ -241,4 +367,6 @@ void setup()
     );
 }
 
-void loop(){}
+void loop(){
+    server.handleClient();
+}
