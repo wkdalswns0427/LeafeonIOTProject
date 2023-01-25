be9a3eeb8ec2b052b0e9a726f49c037af026ab3b
charmander1024 <68832065+wkdalswns0427@users.noreply.github.com>
2023-01-25 15:10:41 +0900

    d

--- Arduino-IDE/DEMO/IND_TEST/IND_TEST.ino
+++ Arduino-IDE/DEMO/IND_TEST/IND_TEST.ino
@@ -13,6 +13,7 @@
 #include <WebServer.h>
 #include <ESPmDNS.h>
 #include <Update.h>
+#include "config.h"
 
 #define RUNNING_CORE 1
 #define BASE_CORE 0
@@ -24,13 +25,6 @@
 #define SCREEN_HEIGHT 64
 #define OLED_RESET     -1
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-const char* host = "esp32";
-const char* ssid = "*********";        //U+Net850C
-const char* password = "********";    //C87568BJ$F//12345678///csdowu38
-const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = 32400;
-const int   daylightOffset_sec = 0;
 
 // elements of SEN0335 : I2C
 typedef DFRobot_BME280_IIC    BME;   
---
+++ Arduino-IDE/DEMO/IND_TEST/config.h
@@ -0,0 +1,6 @@
+const char* host = "esp32";
+const char* ssid = "********";        //U+Net850C
+const char* password = "********";    //C87568BJ$F//12345678///csdowu38
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 32400;
+const int   daylightOffset_sec = 0;
