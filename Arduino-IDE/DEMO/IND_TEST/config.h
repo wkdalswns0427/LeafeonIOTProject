812369125a849031b24d745c6324354192d71984
charmander1024 <68832065+wkdalswns0427@users.noreply.github.com>
2023-01-27 18:29:57 +0900

    crc

--- Arduino-IDE/DEMO/IND_TEST/config.h
+++ Arduino-IDE/DEMO/IND_TEST/config.h
@@ -1,3 +1,11 @@
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef char s8;
+typedef short s16;
+typedef int s32;
+
+
 const char* host = "esp32";
 const char* ssid = "**********";        //U+Net850C
 const char* password = "**********";    //C87568BJ$F//12345678///csdowu38
