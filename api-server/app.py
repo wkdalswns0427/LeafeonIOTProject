2c21561e7d8ec212bb56deecf826bded67e38795
charmander1024 <68832065+wkdalswns0427@users.noreply.github.com>
2023-02-06 17:31:44 +0900

    api and frontend

---
+++ api-server/app.py
@@ -0,0 +1,32 @@
+import uvicorn
+import paho.mqtt.client as mqtt
+from fastapi_mqtt.fastmqtt import FastMQTT
+from fastapi_mqtt.config import MQTTConfig
+from fastapi import FastAPI
+from model import Item, DiscountItem, IsDiscount
+from utils import dataHandler
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+mqtt_config = MQTTConfig()
+fast_mqtt = FastMQTT(config=mqtt_config)
+fast_mqtt.init_app(app)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+async def first():
+    hi = "Hello, Leafeon"
+    return hi
+
+# display sensor data
+@app.post("/sensordata")
+async def postsensordata():
+    
+    return 0
