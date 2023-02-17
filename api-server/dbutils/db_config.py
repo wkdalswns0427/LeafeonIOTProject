018fd95952d6f5f8446d40a26ed3f152cb67e64c
MinJun <68832065+wkdalswns0427@users.noreply.github.com>
2023-02-17 13:19:38 +0900

    Update db_config.py

--- api-server/dbutils/db_config.py
+++ api-server/dbutils/db_config.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 class db_config:
     host: str = "db-leafeon.cp1bodcsjnqi.us-west-1.rds.amazonaws.com"
     port : int = 3306
-    username : str = "wkdaslwns0427"
-    password : str = "pawpaw0426"
+    username : str = "*********"
+    password : str = "******"
     database : str = "leafeon_db"
 
