2f3ed9815c12183b8fe8b125a8c07fc6287d8dab
charmander1024 <68832065+wkdalswns0427@users.noreply.github.com>
2023-03-23 16:40:02 +0900

    pie-chart

--- frontend/src/dashboard.js
+++ frontend/src/dashboard.js
@@ -1,4 +1,5 @@
 const url_get = 'http://localhost:8000/getLatestData'
+var humidity;
 
 async function updateSensorTable() {
     const response = await fetch(url_get);
@@ -17,6 +18,8 @@ async function updateSensorTable() {
         row.insertCell().textContent = dataPoint.pm01data;
         row.insertCell().textContent = dataPoint.pm25data;
         row.insertCell().textContent = dataPoint.pm10data;
+
+        humidity = dataPoint.humidata;
     }
     console.log(sensorData)
 
@@ -27,3 +30,33 @@ async function updateSensorTable() {
 // Call the updateSensorTable function initially and every 5 seconds thereafter
 updateSensorTable();
 setInterval(updateSensorTable, 5000);
+
+
+ function color1(i, classname,colorname){
+    $(classname).css({
+         "background":"conic-gradient("+colorname+" 0% "+i+"%, #ffffff "+i+"% 100%)"
+    });
+ }
+
+ $(window).ready(function(){
+    draw(humidity, '.pie-chart1', '#ccc');
+//    draw(50, '.pie-chart2', '#8b22ff');
+//    draw(30, '.pie-chart3','#ff0');
+ });
+
+ function draw(max, classname, colorname){
+    var i=1;
+     var func1 = setInterval(function(){
+       if(i<max){
+           color1(i,classname,colorname);
+           i++;
+       } else{
+         clearInterval(func1);
+       }
+     },10);
+ }
+ function color1(i, classname,colorname){
+    $(classname).css({
+         "background":"conic-gradient("+colorname+" 0% "+i+"%, #ffffff "+i+"% 100%)"
+    });
+ }
